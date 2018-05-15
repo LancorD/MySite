9a7b8fe584a159b2d9b2592fd30e950bcc11d503
LancorD <s-medium@ya.ru>
2018-05-15 22:24:47 +0700

    more commit

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,14 @@
-var lg = getElementsByClassName('logo');
 
-lg.onclick = function () {
-	this.style.height = '10%';
-	logo.style.top = '0%';
-	logo.style.fontSize = '4em';
+
+const lg = function () {
+	const hd = document.getElementById('cla');
+	hd.style.height = '10%';
+	hd.style.transition = 'all 0.75s';
+	const push = document.getElementsByClassName('logo');
+	for (let i = 0; i < push.length; i++) {
+		push[i].style.top = '0%';
+		push[i].style.fontSize = '4em';
+		push[i].style.transition = 'all 1s';
+	}
+	
 }
