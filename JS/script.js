124027e38cdceca8eabfed319317ece5cd08e5d9
LancorD <s-medium@ya.ru>
2018-05-15 22:34:27 +0700

    !

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,9 @@
 const lg = function () {
-	const hd = document.getElementById('cla');
+	const hd = document.querySelector('header');
+	const push = document.querySelector('.logo');
 	hd.style.height = '10%';
 	hd.style.transition = 'all 0.75s';
-	const push = document.getElementsByClassName('logo');
-	for (let i = 0; i < push.length; i++) {
-		push[i].style.top = '0%';
-		push[i].style.fontSize = '4em';
-		push[i].style.transition = 'all 1s';
-	}
-	
+	push.style.top = '0%';
+	push.style.fontSize = '4em';
+	push.style.transition = 'all 1s';
 }
