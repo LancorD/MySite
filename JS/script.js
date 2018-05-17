e5761b6e27cae9af265b1fa6529a86fa9a49d148
LancorD <s-medium@ya.ru>
2018-05-17 23:31:05 +0700

    !

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,6 @@ const lg = function () {
 	const push = document.querySelector('.logo');
 	hd.style.height = '10%';
 	hd.style.transition = 'all 0.7s';
-	push.style.top = '0%';
-	push.style.fontSize = '4em';
+	push.style.transform = 'translateX(0px)';
 	push.style.transition = 'all 1s';
 }
